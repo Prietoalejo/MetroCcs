ae87c4609e20993adf04f0806cfa4896abea48da
Prietoalejo <alejandrorpu@hotmail.com>
2024-10-19 23:00:36 -0400

    Menu y botones

--- MetroCcs/src/intefaz/Menu.java
+++ MetroCcs/src/intefaz/Menu.java
@@ -3,18 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package intefaz;
-
+import edd.*;
 /**
  *
  * @author guante
  */
 public class Menu extends javax.swing.JFrame {
+    
+    static Grafo test;
 
     /**
      * Creates new form Menu
      */
-    public Menu() {
+    public Menu(Grafo g) {
+        
         initComponents();
+        this.test = g;
+        this.setVisible(true);
     }
 
     /**
@@ -28,11 +33,11 @@ public class Menu extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        addStation = new javax.swing.JButton();
+        AddArista = new javax.swing.JButton();
+        showGrafo = new javax.swing.JButton();
+        addSucursal = new javax.swing.JButton();
+        changeT = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -43,34 +48,34 @@ public class Menu extends javax.swing.JFrame {
         jLabel1.setText("MENU INICIAL");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 30, 240, -1));
 
-        jButton1.setText("AGREGAR ESTACION");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        addStation.setText("AGREGAR ESTACION");
+        addStation.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                addStationActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 130, -1, -1));
+        jPanel1.add(addStation, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 130, -1, -1));
 
-        jButton2.setText("AGREGAR ARISTA");
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 180, -1, -1));
+        AddArista.setText("AGREGAR ARISTA");
+        jPanel1.add(AddArista, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 180, -1, -1));
 
-        jButton3.setText("MOSTRAR");
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 230, -1, -1));
+        showGrafo.setText("MOSTRAR");
+        jPanel1.add(showGrafo, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 230, -1, -1));
 
-        jButton4.setText("COLOCAR SUCURSAL");
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 280, -1, -1));
+        addSucursal.setText("COLOCAR SUCURSAL");
+        jPanel1.add(addSucursal, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 280, -1, -1));
 
-        jButton5.setText("CAMBIAR \"T\"");
-        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 330, -1, -1));
+        changeT.setText("CAMBIAR \"T\"");
+        jPanel1.add(changeT, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 330, -1, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 480));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void addStationActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addStationActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_addStationActionPerformed
 
     /**
      * @param args the command line arguments
@@ -102,18 +107,18 @@ public class Menu extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Menu().setVisible(true);
+                new Menu(test).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton AddArista;
+    private javax.swing.JButton addStation;
+    private javax.swing.JButton addSucursal;
+    private javax.swing.JButton changeT;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton showGrafo;
     // End of variables declaration//GEN-END:variables
 }
--- MetroCcs/src/metroccs/Main.java
+++ MetroCcs/src/metroccs/Main.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package metroccs;
+import intefaz.*;
+import edd.*;
 
 /**
  *
@@ -14,7 +16,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Grafo g = new Grafo(1);
+       Menu test = new Menu(g);
+
     }
     
 }
